265b779f873da0b8032795e86e08ee848d3de982
lixiaolun <123456@qq.com>
2019-11-16 10:56:15 +0800

    实现批量删除

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -81,7 +81,7 @@ $('#modifyBox').on('submit','#userForm',function () {
 //删除数据 
 $('#userBox').on('click','.delete',function () {
   let id = $(this).attr('data-id'); 
-  console.log(id); 
+  // console.log(id); 
   if (confirm('确定删除')) {
     $.ajax({
       type: "delete",
@@ -92,4 +92,59 @@ $('#userBox').on('click','.delete',function () {
     });
   }
   
+})
+//批量删除 
+//单复选框
+let selectAll = $('#selectAll');
+selectAll.on('change',function () {
+  let status = $(this).prop('checked') 
+//  alert(status)
+  $('#userBox').find('input').prop('checked', status);
+  //批量删除按钮的显示隐藏
+  if (status) {
+    $('#deleteMany').show();
+  } else {
+    $('#deleteMany').hide();
+  }
+})
+
+$('#userBox').on('change','.userStatus',function () {
+  // alert(1)
+  let inputs = $('#userBox').find('input'); 
+  // console.log(inputs);
+  if (inputs.length === inputs.filter(':checked').length) {
+    selectAll.prop('checked', true);
+  } else {
+    selectAll.prop('checked', false);
+  }
+  //批量删除按钮的显示隐藏
+  if (inputs.filter(':checked').length > 0) {
+    $('#deleteMany').show();
+  } else {
+    $('#deleteMany').hide();
+  }
+})
+//实现批量删除
+$('#deleteMany').on('click',function () {
+  var checkedUser = $('#userBox').find('input').filter(':checked') 
+  // console.log(checkedUser);
+  var ids = []; 
+  checkedUser.each(function (index, item) {
+    // console.log($(item).attr('data-id'));
+    ids.push($(item).attr('data-id')) //注意这里的item是原生js需要转换为jquery  其次需要在对应的input里面添加data-id
+  });
+  console.log(ids); 
+
+  if (confirm('确定批量删除')) {
+    $.ajax({
+      type: 'delete',
+      url: `/users/${ids.join('-')}`,
+      success: () => {
+        location.reload()
+      }
+     })
+   }
+
+
+
 })
